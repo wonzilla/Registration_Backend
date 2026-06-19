8f6c93131f0d624a3b9c49b995eed74e744fe9c7
M Farhan <tahqeeq86@gmail.com>
2026-06-19 18:12:55 +0500

    database port updated

--- config/database.js
+++ config/database.js
@@ -5,8 +5,10 @@ const sequelize = new Sequelize(
     process.env.MYSQL_DATABASE || 'physical_registrations',
     process.env.MYSQL_USER || 'root',
     process.env.MYSQL_PASSWORD || '',
+    
     {
         host: process.env.MYSQL_HOST || 'localhost',
+         port: process.env.MYSQL_PORT,
         dialect: 'mysql',
         logging: false,
         pool: {
