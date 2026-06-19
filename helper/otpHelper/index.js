fe1a94a193e9fa80f56d69244a0ae655f2e1ce63
M Farhan <tahqeeq86@gmail.com>
2026-06-19 23:53:51 +0500

    gmail config updated

--- helper/otpHelper/index.js
+++ helper/otpHelper/index.js
@@ -1,33 +1,40 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const sendOTPEmail = async (to, mailOptions) => {
-   const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-auth: {
-            user: "smacademy74@gmail.com", 
-            pass: "dlpe okze cvub ctbu"  
-        }
-});
+  console.log("sending email");
 
-await transporter.verify();
-console.log("SMTP Ready");
+  const transporter = nodemailer.createTransport({
+    host: "74.125.136.108", // smtp.gmail.com ka IPv4 test
+    port: 587,
+    secure: false,
+    auth: {
+      user: "smacademy74@gmail.com",
+      pass: "dlpe okze cvub ctbu",
+    },
+    tls: {
+      servername: "smtp.gmail.com",
+    },
+  });
 
-     const mailConfig = {
-        from: '"Sirat Ul Mustaqeem Academy" <smacademy74@gmail.com>',
-        to: to,
-        subject: mailOptions.subject,  // Directly access the properties
-        html: mailOptions.html        // Directly access the properties
-    };
+  console.lot("transport config cleared now sending");
 
-    try {
-        await transporter.sendMail(mailConfig);
-        return true;
-    } catch (error) {
-        console.error("Error sending email:", error);
-        throw error;
-    }
+  await transporter.verify();
+  console.log("SMTP Ready");
+
+  const mailConfig = {
+    from: '"Sirat Ul Mustaqeem Academy" <smacademy74@gmail.com>',
+    to: to,
+    subject: mailOptions.subject, // Directly access the properties
+    html: mailOptions.html, // Directly access the properties
+  };
+
+  try {
+    await transporter.sendMail(mailConfig);
+    return true;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
 };
 
 module.exports = sendOTPEmail; // ✅ export is correct
